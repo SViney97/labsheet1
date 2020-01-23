eadca225ef210ae378c827b1caa8fb321d65adc6
S00186271 <S00186271@LAB-B1042-01>
2020-01-23 16:11:37 +0000

    week1-labsheet1

--- labsheet1/Band.cs
+++ labsheet1/Band.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace labsheet1
 {
-    class Band
+    public abstract class Band : IComparable
     {
+        public string BandName { get; set; }
+
+        public int YearFormed { get; set; }
+
+        public string Members { get; set; }
+
+
+        public Band()
+        {
+
+        }
+
+        public Band(string bandname, int yearformed, string members)
+        {
+            BandName = bandname;
+            YearFormed = yearformed;
+            Members = members;
+            
+        }
+
+        public int CompareTo(object obj)
+        {
+            //get name for thing beside current activity
+            //this sorts the data beside it.
+            //for example if the data was C,A,B - it will sort itself to form A,B,C.
+            Band objectBeside = obj as Band;
+            return this.BandName.CompareTo(objectBeside.BandName);
+        }
+        public override string ToString()
+        {
+          
+            return $"{BandName}";
+        }
+
+        
+
+        
+    }//end of class
+
+
+
+    public class RockBand : Band
+    {
+
+    }
+
+    public class PopBand : Band
+    {
+
+    }
+
+    public class IndieBand : Band
+    {
+
+    }
+
+    public class album
+    {
+
     }
 }
--- labsheet1/MainWindow.xaml.cs
+++ labsheet1/MainWindow.xaml.cs
@@ -20,9 +20,77 @@ namespace labsheet1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Band> Bands= new List<Band>();
         public MainWindow()
         {
             InitializeComponent();
+
+
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            RockBand band1 = new RockBand()
+            {
+                BandName = "Panic at the disco",
+                YearFormed = 2006,
+                Members = "Brendan Uire"
+            };
+
+            IndieBand band2 = new IndieBand()
+            {
+                BandName = "fall out boy",
+                YearFormed = 2000,
+                Members = "tom"
+            };
+
+             RockBand band3 = new RockBand()
+            {
+                BandName = "linkin park",
+                YearFormed = 2000,
+                Members = "mike,chester"
+            };
+            PopBand band4 = new PopBand()
+            {
+                BandName = "genesis",
+                YearFormed = 1967,
+                Members = "phil collins,"
+            };
+            PopBand band5 = new PopBand()
+            {
+                BandName = "abba",
+                YearFormed = 1976,
+                Members = "tina"
+            };
+            RockBand band6 = new RockBand()
+            {
+                BandName = "Queen",
+                YearFormed = 1970,
+                Members = "freddy"
+            };
+
+            Bands.Add(band1);
+            Bands.Add(band2);
+            Bands.Add(band3);
+            Bands.Add(band4);
+            Bands.Add(band5);
+            Bands.Add(band6);
+
+            RefreshList();
+            lbxBand.ItemsSource = Bands;
+        }
+        private void RefreshList()
+        {
+            lbxBand.ItemsSource = null;
+            lbxBand.ItemsSource = Bands;
+
+  
+            //sorts the bands in bands listbox by band name 
+            //display in order
+            Bands.Sort();
+            
+        }//end of RefreshList
+
     }
+
 }
